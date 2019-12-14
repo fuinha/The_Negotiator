b2b5b5ad4808dcf30a3b9d6c2327afb7e39b559c
Michal Terranova <michalterranova@mail.com>
2019-12-14 10:49:42 -0500

    saved v.3

--- public/js/viewapp.js
+++ public/js/viewapp.js
@@ -73,10 +73,73 @@ console.log(data)
         window.location.href = "/service";
       });
     });
+
+    '<input type="text" id="year_business" value="'++'">'
+    '<input type="text" id="yearDealer" value="">'
+    '<input type="text" id="address" value="">'
+    '<input type="text" id="prev_names" value="">'
+    '<input type="text" id="" value="'++''">'
+    '<input type="text" id="" value="">'
+    '<input type="text" id="" value="">'
+    '<input type="text" id="" value="">'
+    '<input type="text" id="" value="">'
+    '<input type="text" id="" value="">'
+    '<input type="text" id="" value="">'
+    '<input type="text" id="" value="">'
+    '<input type="text" id="" value="">'
+    '<input type="text" id="" value="">'
+    '<input type="text" id="" value="">'
+    '<input type="text" id="" value="">'
+    '<input type="text" id="" value="">'
+    '<input type="text" id="" value="">'
+    '<input type="text" id="" value="">'
+
+
+
   
+  var year_business = $("#year_business").val().trim();
+  var yearDealer = $("#yearDealer").val();
+  var address = $("#address").val().trim();
+  var prev_names = $("#prev_names").val().trim();
+  var prev_years = $("#prev_years").val().trim();
+  var claims = $("#claims").val().trim();
+  var employee = $("#employee").val().trim();
+  var license = $("#license").val().trim();
+  var dob = $("#dob").val();
+  var furnished = $("#furnished").val();
+  var employee = $("#employee").val();
+  var accidents = $("#accidents").val();
+  var business_type = $("#business_type").val();
+  var insurance = $("#insurance").val();
+  var miles = $("#miles").val().trim();
+  var time = $("#time").val();
+  var lot = $("#lot").val().trim();
+  var worth = $("#worth").val().trim();
+  var plates = $("#plates").val().trim();
+  var security = $("#security").val();
 
-
-
+  var editApp = {
+      years_inBusiness: year_business,
+      businessOpen_years: yearDealer,
+      additional_addresses: address,
+      previous_insuranceName: prev_names,
+      previous_insuranceYears: prev_years,
+      lot_protection: security,
+      num_employees: employee,
+      previous_insuranceClaims: claims,
+      drivers_license: license,
+      dob_employees: dob,
+      furnished: furnished,
+      employee_title: time,
+      recent_accidents: accidents,
+      business_nature: business_type,
+      insurance_type: insurance,
+      miles_driven: miles,
+      num_lotCars: lot,
+      lot_worth: worth,
+      num_plates: plates
+}
+console.log(editApp)
 
     
 $("#update").on("click", function() {
