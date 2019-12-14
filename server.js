ce140ef31c77c1556e187a748e4df14385ac9ec0
Michal Terranova <michalterranova@mail.com>
2019-12-13 22:05:43 -0500

    user_data added

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ require("./routes/html-routes.js")(app);
 
 //syncing sequelize models and starting express app
 
-db.sequelize.sync({}).then( function() {
+db.sequelize.sync({force: true}).then( function() {
     app.listen(PORT, function() {
         console.log("==> 🌎 App listening on PORT "+ PORT);
     });
